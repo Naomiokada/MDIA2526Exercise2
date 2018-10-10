8d97f2faac0ed66b110406ebb83bbafbd526d835
Naomi Okada <naomiokada@naomis-mbp-2.wifi.bcit.ca>
2018-10-09 19:48:24 -0700

    added text under function

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ app.style.backgroundColor = "rgb(255,222,255)";
 }, 500);
 
 //B
-function RandomBlue (){
+function RandomBlue(){
+
+var blue = Math.round(Math.random()*255)
+return "rgb(100, 100, "+blue+")";
+
 
 
 }
